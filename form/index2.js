6600032f852fed462faf0f097fc6c72951d7745f
ameer <tmsyameersuhail@gmail.com>
2021-10-02 10:18:15 -0700

    ReSubmit_SPS

---
+++ form/index2.js
@@ -0,0 +1,77 @@
+
+    $("#registration_form").validate({
+        rules:{
+            Full_name:{
+                required:true
+            },
+            Place:{
+                required:true
+            },
+            age:{
+                required:true,
+                number:true
+            },
+            Email:{
+                required:true
+            },
+            Contact_no:{
+                required:true,
+                number:true,
+                minlength:10
+            },
+            employment_status:{
+                required:true
+            },
+            education:{
+                required:true
+            },
+            organisation:{
+                required:true
+            },
+            graduation:{
+                required:true
+            },
+            tell_us:{
+                required:true,
+                minlength:5
+            },
+            join_sps:{
+                required:true
+            },
+            motivates:{
+                required:true
+            },
+            know_sps:{
+                required:true
+            },
+            agree:{
+                required:true
+            }
+    
+    
+    
+            
+        },
+        messages:{
+            Full_name:{
+                required: 'Full name is mandatory!'
+            }
+        },
+        submitHandler:function(form){
+            $.ajax({
+                url:"https://script.google.com/macros/s/AKfycbxuPNMdOdxt0LcO93zq_mSkK57WCJ5AybY4HJP3HZ1NaRKyRkQqRBY8kuzvQL4T2MbR/exec",
+                data:$("#registration_form").serialize(),
+                method:"post",
+                success:function (response){
+                    alert("Message submitted successfully")
+                    window.location.reload()
+                },
+                error:function (err){
+                    alert("Something Error")
+                }
+            })
+        }
+    })
+    
+    
+    
